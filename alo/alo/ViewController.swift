0ce3ec78122838dea767791fe9cad18c62d9769d
Turma Manhã <turma.manha@172-16-3-29.0000.ufpe.br.iosda.org>
2018-04-03 11:22:02 -0300

    trivia app

---
+++ alo/alo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alo
+//
+//  Created by Turma Manhã on 03/04/18.
+//  Copyright © 2018 Turma Manhã. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
